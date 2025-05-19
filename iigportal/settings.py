f4d94353b457d861ea13fe70be32e160908dfe0c
Md Mohibul Hasan <55809731+mohibulhasan@users.noreply.github.com>
2025-05-19 12:20:09 +0100

    Solving issue

--- iigportal/settings.py
+++ iigportal/settings.py
@@ -137,8 +137,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'iigportal.bsccl@gmail.com'
-EMAIL_HOST_PASSWORD = 'fpciwnjiofvmhxen' #changing password
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = '' #changing password
 #Google APP password: fpciwnjiofvmhxen
 # MESSAGE_TAGS = {
 #     messages.INFO: 'alert-info',
